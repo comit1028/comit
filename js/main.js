f8ccecc8d31b9929c1ccd328cfff86dac4062d70
Aptheparker <aptheparker@gmail.com>
2022-12-28 12:04:20 +0900

    js changed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const mainNav = document.querySelector('.main-nav');
+const x = document.querySelector(".icon-mobile-nav[name='close-outline']");
+const o = document.querySelector(".icon-mobile-nav[name='menu-outline']");
 
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
+btnNavEl.addEventListener('click', function () {
+  if (mainNav.style.display === 'block') {
+    mainNav.style.display = 'none';
+    x.style.display = 'none';
+    o.style.display = 'block';
+  } else {
+    mainNav.style.display = 'block';
+    x.style.display = 'block';
+    o.style.display = 'none';
+  }
 });
